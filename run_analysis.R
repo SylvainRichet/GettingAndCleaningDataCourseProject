1973dcc632df86a3ccd1981f23bfb5d56e7017bf
SylvainRichet <sylvain.richet@easytrust.com>
2016-01-31 18:33:54 +0100

    Read elements and merge data before extracting mean() and std()

--- run_analysis.R
+++ run_analysis.R
@@ -5,11 +5,13 @@
 #
 # *****************************************************************************
 
-# ... libraries will be used
+# these libraries will be used
+library(data.table)
 
 # Directories
-rawDataDir =  paste(getwd(), "raw-data", sep = "/")
-tidyDataDir =  paste(getwd(), "tidy-data", sep = "/")
+rawDataDir <- file.path(getwd(), "raw-data")
+tidyDataDir <- file.path(getwd(), "tidy-data")
+rootRawData <- file.path(rawDataDir, "UCI HAR Dataset")
 
 
 # Download Raw data zip file
@@ -19,4 +21,27 @@ download.file(url, rawDataZipFile)
 
 # unzip file
 unzip(zipfile = rawDataZipFile, exdir = rawDataDir )
+
+# Read Subjects for both populations
+dtSubjectTrain <- data.table(fread(file.path(rootRawData, "train", "subject_train.txt")))
+dtSubjectTest <- data.table(fread(file.path(rootRawData, "test", "subject_test.txt")))
+
+# Read Activities for both populations
+dtActivityTrain <- data.table(fread(file.path(rootRawData, "train", "y_train.txt")))
+dtActivityTest <- data.table(fread(file.path(rootRawData, "test", "y_test.txt")))
+
+# Finally read the DATA for both populations
+dtTrain <- data.table(fread(file.path(rootRawData, "train", "X_train.txt")))
+dtTest <- data.table(fread(file.path(rootRawData, "test", "X_test.txt")))
+
+# Combine all Subjects, Activities and DATA
+dtSubjects <- rbind(dtSubjectTrain, dtSubjectTest)
+setnames(dtSubjects, "subject")
+dtActivities <- rbind(dtActivityTrain, dtActivityTest)
+setnames(dtActivities, "activity")
+dtDatas <- rbind(dtTrain,dtTest)
+
+# Finally combine all and sort
+dtDatas <- cbind(dtSubjects, dtActivities, dtDatas)
+setkey(dtDatas, subject, activity)
 
