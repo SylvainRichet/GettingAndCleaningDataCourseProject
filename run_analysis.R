f10adeeb143721575b70efd7c6290709b0e85e58
SylvainRichet <sylvain.richet@easytrust.com>
2016-01-31 17:28:33 +0100

    Getting and unzipping raw data

--- run_analysis.R
+++ run_analysis.R
@@ -5,3 +5,18 @@
 #
 # *****************************************************************************
 
+# ... libraries will be used
+
+# Directories
+rawDataDir =  paste(getwd(), "raw-data", sep = "/")
+tidyDataDir =  paste(getwd(), "tidy-data", sep = "/")
+
+
+# Download Raw data zip file
+rawDataZipFile <- file.path(rawDataDir, "rawdata.zip")
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, rawDataZipFile)
+
+# unzip file
+unzip(zipfile = rawDataZipFile, exdir = rawDataDir )
+
